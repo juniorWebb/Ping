2ba1e29765426a9045b8598d89e218db8a089c00
Nsisong <singdsong242@gmail.com>
2023-11-01 21:55:52 +0000

    first commit

---
+++ validation.js
@@ -0,0 +1,10 @@
+const form= document.querySelector('#form');
+const email= document.querySelector('#email');
+const error= document.querySelector('.error');
+
+
+form.addEventListener('submit', (e) => {
+  
+  e.preventDefault()
+
+})
