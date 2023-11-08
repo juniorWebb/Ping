7224acf4e0c19ded4926f18aff40dce63ab7d658
Nsisong <singdsong242@gmail.com>
2023-11-08 14:41:11 +0000

    js form validation and minor adjustment

--- validation.js
+++ validation.js
@@ -1,10 +1,32 @@
-const form= document.querySelector('#form');
-const email= document.querySelector('#email');
-const error= document.querySelector('.error');
+const error = document.querySelector(".error");
+const email = document.querySelector("#email");
 
+function validateEmail() {
 
-form.addEventListener('submit', (e) => {
-  
-  e.preventDefault()
+  if (!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    error.innerHTML = "Please provide a valid email address";
+    error.style.display = "block";
+    error.style.color = "var(--lightRed)";
+    email.style.borderColor = "var(--lightRed)";
+    return false;
+  }
 
-})
+  error.innerHTML = "You are good to GO!";
+  error.style.display = "block";
+  error.style.color = "green";
+  email.style.borderColor = "green";
+  return true;
+}
+
+function formValidation() {
+  if (!validateEmail()) {
+    error.innerHTML = "Whoops! It looks like you forgot to add your email";
+    email.style.borderColor = "var(--lightRed)";
+    error.style.display = "block";
+    setTimeout(() => {
+      error.style.display = "none";
+      email.style.borderColor = "var(--paleBlue)";
+    }, 3000);
+    return false;
+  }
+}
